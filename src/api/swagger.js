4012c370cf862b362df1967740beb395941f085b
linhe <linhe@dtstack.com>
2019-02-21 15:37:03 +0800

    feat(更新构建工具):

--- src/api/swagger.js
+++ src/api/swagger.js
@@ -1,45 +1,114 @@
 export default {
-    // ==== DT-BootX Default Index Controller ==== //
-    indexUsingGET: { method: 'get', url: `/` }, // 根目录Controller
+    /* -------- DT-BootX Default Index Controller -------- */
+    indexUsingGet: { // 根目录Controller
+        method: 'get',
+        url: `/` 
+    }, 
 
-    // ==== 智慧门店 Store Controller ==== //
-    getManageOverViewDataUsingGET: { method: 'get', url: `/api/v1/shop/manage-overview-data` }, // 经营概览
+    /* -------- 智慧门店 Store Controller -------- */
+    getManageOverViewDataUsingGet: { // 经营概览
+        method: 'get',
+        url: `/api/v1/shop/manage-overview-data` 
+    }, 
 
-    // ==== 智慧门店 - 导购分析 Guider Analysis Controller ==== //
-    getGuiderMonthRankUsingGET: { method: 'get', url: `/api/v1/guider-analysis/month-rank` }, // 导购销售月排名
-    getTodaySaleUsingGET: { method: 'get', url: `/api/v1/guider-analysis/today-sale` }, // 今日导购销售
+    /* -------- 智慧门店 - 导购分析 Guider Analysis Controller -------- */
+    getGuiderMonthRankUsingGet: { // 导购销售月排名
+        method: 'get',
+        url: `/api/v1/guider-analysis/month-rank` 
+    }, 
+    getTodaySaleUsingGet: { // 今日导购销售
+        method: 'get',
+        url: `/api/v1/guider-analysis/today-sale` 
+    }, 
 
-    // ==== 智慧门店 - 引流分析 Drainage Analyse Controller ==== //
-    getDrainageAnalyseDataUsingGET: { method: 'get', url: `/api/v1/drainage-analyse/drainage-analyse-data` }, // 游戏互动人数
-    exportDrainageAnalyseDataUsingGET: { method: 'get', url: `/api/v1/drainage-analyse/drainage-analyse-data/export-excel` }, // 引流分析数据Excel导出
-    getMutCntTrendUsingGET: { method: 'get', url: `/api/v1/drainage-analyse/get-interactiveAmount-trend` }, // 客流人数趋势
-    getSaleMoneyAmountTrendUsingGET: { method: 'get', url: `/api/v1/drainage-analyse/get-saleMoneyAmount-trend` }, // 互动人数趋势
+    /* -------- 智慧门店 - 引流分析 Drainage Analyse Controller -------- */
+    getDrainageAnalyseDataUsingGet: { // 游戏互动人数
+        method: 'get',
+        url: `/api/v1/drainage-analyse/drainage-analyse-data` 
+    }, 
+    exportDrainageAnalyseDataUsingGet: { // 引流分析数据Excel导出
+        method: 'get',
+        url: `/api/v1/drainage-analyse/drainage-analyse-data/export-excel` 
+    }, 
+    getMutCntTrendUsingGet: { // 客流人数趋势
+        method: 'get',
+        url: `/api/v1/drainage-analyse/get-interactiveAmount-trend` 
+    }, 
+    getSaleMoneyAmountTrendUsingGet: { // 互动人数趋势
+        method: 'get',
+        url: `/api/v1/drainage-analyse/get-saleMoneyAmount-trend` 
+    }, 
 
-    // ==== 智慧门店 - 热力动线 Hot Area Controller ==== //
-    queryHotLineUsingPOST: { method: 'post', url: `/api/v1/hotArea/hot-line` }, // 热力动线
+    /* -------- 智慧门店 - 热力动线 Hot Area Controller -------- */
+    queryHotLineUsingPost: { // 热力动线
+        method: 'post',
+        url: `/api/v1/hotArea/hot-line` 
+    }, 
 
-    // ==== 智慧门店 - 逛店分析 In Store Analysis Controller ==== //
-    queryAgeDistributeUsingGET: { method: 'get', url: `/api/v1/shop/in-shop-analysis/age-distribute` }, // 女性-年龄分布/男性-年龄分布
-    exportProductBuyAnalysisUsingGET: { method: 'get', url: `/api/v1/shop/in-shop-analysis/export-product-buy-analysis` }, // 商品购买分析导出
-    productBuyAnalysisUsingPOST: { method: 'post', url: `/api/v1/shop/in-shop-analysis/product-buy-analysis` }, // 商品购买分析
+    /* -------- 智慧门店 - 逛店分析 In Store Analysis Controller -------- */
+    queryAgeDistributeUsingGet: { // 女性-年龄分布/男性-年龄分布
+        method: 'get',
+        url: `/api/v1/shop/in-shop-analysis/age-distribute` 
+    }, 
+    exportProductBuyAnalysisUsingGet: { // 商品购买分析导出
+        method: 'get',
+        url: `/api/v1/shop/in-shop-analysis/export-product-buy-analysis` 
+    }, 
+    productBuyAnalysisUsingPost: { // 商品购买分析
+        method: 'post',
+        url: `/api/v1/shop/in-shop-analysis/product-buy-analysis` 
+    }, 
 
-    // ==== 智慧门店-门店信息 Store Info Controller ==== //
-    getShopInfoUsingGET: { method: 'get', url: `/api/v1/shop-info/shop` }, // getShopInfo
+    /* -------- 智慧门店-门店信息 Store Info Controller -------- */
+    getShopInfoUsingGet: { // getShopInfo
+        method: 'get',
+        url: `/api/v1/shop-info/shop` 
+    }, 
 
-    // ==== 消费者概述 Cust Controller ==== //
-    exportUserTagRatioUsingGET: { method: 'get', url: `/api/v1/cust/export-usertag-ratio` }, // 导出用户标签占比数据
-    getAreaTreeUsingGET: { method: 'get', url: `/api/v1/cust/get-area-tree` }, // 获取区域树
-    getTagClassUsingGET: { method: 'get', url: `/api/v1/cust/get-tags-class` }, // 获取用户标签类别
-    getUserTagRatioUsingPOST: { method: 'post', url: `/api/v1/cust/get-usertag-ratio` }, // 获取用户标签占比
+    /* -------- 消费者概述 Cust Controller -------- */
+    exportUserTagRatioUsingGet: { // 导出用户标签占比数据
+        method: 'get',
+        url: `/api/v1/cust/export-usertag-ratio` 
+    }, 
+    getAreaTreeUsingGet: { // 获取区域树
+        method: 'get',
+        url: `/api/v1/cust/get-area-tree` 
+    }, 
+    getTagClassUsingGet: { // 获取用户标签类别
+        method: 'get',
+        url: `/api/v1/cust/get-tags-class` 
+    }, 
+    getUserTagRatioUsingPost: { // 获取用户标签占比
+        method: 'post',
+        url: `/api/v1/cust/get-usertag-ratio` 
+    }, 
 
-    // ==== 用户登录 User Login Controller ==== //
-    userLoginUsingPOST: { method: 'post', url: `/api/v1/user/login` }, // 用户登录
-    userLoginOutUsingGET: { method: 'get', url: `/api/v1/user/login-out` }, // 用户登出
-    getUserInfoUsingGET: { method: 'get', url: `/api/v1/user/user-info` }, // 用户信息
+    /* -------- 用户登录 User Login Controller -------- */
+    userLoginUsingPost: { // 用户登录
+        method: 'post',
+        url: `/api/v1/user/login` 
+    }, 
+    userLoginOutUsingGet: { // 用户登出
+        method: 'get',
+        url: `/api/v1/user/login-out` 
+    }, 
+    getUserInfoUsingGet: { // 用户信息
+        method: 'get',
+        url: `/api/v1/user/user-info` 
+    }, 
 
-    // ==== 首页看板 Board Controller ==== //
-    getBoardDataUsingGET: { method: 'get', url: `/api/v1/board/get-board-data` }, // 获取首页看板核心数据
-    getBoardSaleTrendUsingGET: { method: 'get', url: `/api/v1/board/get-board-sale-trend` }, // 获取首页销售流水趋势
-    getBoardSSSGTrendUsingGET: { method: 'get', url: `/api/v1/board/get-board-sssg-trend` }, // 获取首页SSSG趋势
+    /* -------- 首页看板 Board Controller -------- */
+    getBoardDataUsingGet: { // 获取首页看板核心数据
+        method: 'get',
+        url: `/api/v1/board/get-board-data` 
+    }, 
+    getBoardSaleTrendUsingGet: { // 获取首页销售流水趋势
+        method: 'get',
+        url: `/api/v1/board/get-board-sale-trend` 
+    }, 
+    getBoardSSSGTrendUsingGet: { // 获取首页SSSG趋势
+        method: 'get',
+        url: `/api/v1/board/get-board-sssg-trend` 
+    }, 
 
 }
